90f7bd7a9dcb190a1a475883f537da7aad208957
Jacob Helwig <jacob.a.helwig@stat.tamu.edu>
2022-05-02 00:02:01 -0500

    Importance sampling-grid search hybrid experiment; analysis of grid search hyperparameter selection

---
+++ dev/analyses_demos_experiments/hyperparameter_specification/optimal_pi_analysis2_20var.R
@@ -0,0 +1,108 @@
+start <- Sys.time()
+library(covdepGE)
+library(ggplot2)
+library(doParallel)
+library(doRNG)
+
+# function for generating the data and the covariates
+generate_continuous <- function(n1 = 60, n2 = 60, n3 = 60, p = 4){
+
+  # create covariate for individuals in each of the three intervals
+
+  # define the dimensions of the data
+  n <- sum(n1, n2, n3)
+
+  # define the limits of the intervals
+  limits1 <- c(-3, -1)
+  limits2 <- c(-1, 1)
+  limits3 <- c(1, 3)
+
+  # define the covariate values within each interval
+  z1 <- runif(n1, limits1[1], limits1[2])
+  z2 <- runif(n2, limits2[1], limits2[2])
+  z3 <- runif(n3, limits3[1], limits3[2])
+  Z <- matrix(sort(c(z1, z2, z3)), n, 1)
+
+  # create precision matrices
+
+  # the shared part of the structure for all three intervals is a 2 on the
+  # diagonal and a 1 in the (2, 3) position
+  common_str <- diag(p + 1)
+  common_str[2, 3] <- 1
+
+  # define constants for the structure of interval 2
+  beta1 <- diff(limits2)^-1
+  beta0 <- -limits2[1] * beta1
+
+  # interval 2 has two different linear functions of Z in the (1, 2) position
+  # and (1, 3) positions; define structures for each of these components
+  int2_str12 <- int2_str13 <- matrix(0, p + 1, p + 1)
+  int2_str12[1, 2] <- int2_str13[1, 3] <- 1
+
+  # define the precision matrices for each of the individuals in interval 2
+  int2_prec <- lapply(z2, function(z) common_str +
+                        ((1 - beta0 - beta1*z)*int2_str12) +
+                        ((beta0 + beta1*z)*int2_str13))
+
+  # interval 1 has a 1 in the (1, 2) and interval 3 has a 1 in the (1, 3) position;
+  # define structures for each of these components
+  int1_str12 <- int3_str13 <- matrix(0, p + 1, p + 1)
+  int1_str12[1, 2] <- int3_str13[1, 3] <- 1
+
+  # define the precision matrices for each of the individuals in interval 1 and interval 3
+  int1_prec <- rep(list(common_str + int1_str12), n1)
+  int3_prec <- rep(list(common_str + int3_str13), n3)
+
+  # put all of the precision matrices into one list
+  prec_mats <- c(int1_prec, int2_prec, int3_prec)
+
+  # symmetrize the precision matrices
+  prec_mats <- lapply(prec_mats, function(mat) t(mat) + mat)
+
+  # invert the precision matrices to get the covariance matrices
+  cov_mats <- lapply(prec_mats, solve)
+
+  # generate the data using the covariance matrices
+  data_mat <- t(sapply(cov_mats, MASS::mvrnorm, n = 1, mu = rep(0, p + 1)))
+
+  return(list(data = data_mat, covts = Z, true_precision = prec_mats))
+}
+
+# how many trials?
+n_trials <- 100
+set.seed(1)
+
+# create the hyperparameter grid
+pip <- c(1e-5, 1e-4, 1e-3, 0.01, 0.025, seq(0.05, 0.95, 0.05), 0.99, 0.999, 0.9999, 0.99999)
+sbsq <- c(1e-5, 1e-4, 1e-3, 0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 1)
+ssq <- c(0.1, 0.5, 1, 3)
+hp_grid <- expand.grid(ssq = ssq, sbsq = sbsq, pip = pip)
+
+# spin up parallel backend
+# find the available number of cores
+(cores <- detectCores() - 5)
+
+registerDoParallel(cores)
+
+res <- foreach(trial_ind = 1:n_trials, .packages = "covdepGE") %dorng%{
+
+  # generate the data
+  dat <- generate_continuous(p = 20 - 1)
+  X <- dat$data
+  Z <- dat$covts
+
+  # fit both models
+  out <- tryCatch(covdepGE(X, Z, ssq = hp_grid$ssq,
+                           sbsq = hp_grid$sbsq, pip = hp_grid$pip, prog_bar = F),
+                  error = function(msg) msg)
+
+  cat("...", trial_ind)
+
+  # return each of the resulting models and the data
+  list(data = dat, out = out)
+}
+
+# save res
+save(res, file = "opt_pi2__20var.Rda")
+Sys.time() - start
+stopImplicitCluster()
