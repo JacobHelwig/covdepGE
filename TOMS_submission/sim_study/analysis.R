330f7f33ae3dea240b5aa8247417e0d8202013d0
JacobHelwig <jacob.a.helwig@gmail.com>
2023-10-09 21:36:41 -0500

    Hypothesis testing, sequential analysis, 4D analysis

--- TOMS_submission/sim_study/analysis.R
+++ TOMS_submission/sim_study/analysis.R
@@ -18,6 +18,8 @@ subgroups_list <- list()
 med <- T
 univariate <- T
 sine <- T
+four <- F
+seq <- F
 if (univariate){
 
   # univariate extraneous covariate
@@ -29,15 +31,18 @@ if (univariate){
   if (sine){
     path <- paste0(path, "_sine")
     exper <- paste0(exper, "sine_")
+  }else if (seq){
+    path <- paste0(path, "_seq")
+    methods <- c("covdepGE", "covdepGE_seq")
   }
 }else{
 
   # multivariate extraneous covariate
-  exper <- "cont_multi_cov_dep_"
+  exper <- ifelse(four, "cont_4_cov_dep_", "cont_multi_cov_dep_")
   methods <- c("covdepGE" , "JGL", "mgm", "covdepGE_sortZ")
-  n <- 25
+  n <- 25 + 200 * four
   samp_str <- paste0("_n_", n)
-  path <- "./experiments/z2"
+  path <- ifelse(four, "./experiments/z4", "./experiments/z2")
 }
 
 ntrials <- 50
@@ -45,8 +50,8 @@ trial_str <- paste0("ntrials", ntrials, "_")
 dims <- c(10, 25, 50, 100)
 files <- list.files(path)
 prec <- 2
-df <- subgroups <- vector("list", length(dims))
-names(df) <- dims
+df <- subgroups <- htest <- vector("list", length(dims))
+names(df) <- names(subgroups) <- names(htest) <- dims
 
 for (p in as.character(dims)){
 
@@ -54,8 +59,9 @@ for (p in as.character(dims)){
   exp_name <- paste0(exper, trial_str, "p", p, samp_str)
 
   # load results
+  # mean(sapply(results, function(trial)length(trial$covdepGE$graphs$unique_graphs)))
   file_name <- files[grepl(exp_name, files) & endsWith(files, ".Rda")]
-  if (length(file_name) != 1) stop(paste(length(file_name), "files found"))
+  if (length(file_name) != 1) stop(paste(length(file_name), "files found for", exp_name))
   file_path <- file.path(path, file_name)
   load(file_path)
   print(paste0("n: ", results$sample_data[1], ", p: ", results$sample_data[2]))
@@ -78,6 +84,23 @@ for (p in as.character(dims)){
   if (med) sens_sd <- sprintf(paste0("%.", prec, "f"), apply(sens * 100, 1, IQR))
   sens_str <- paste0(sens_mean, " (", sens_sd, ")")
 
+  # hypothesis testing
+  if (!seq){
+    t_test <- t.test(sens['covdepGE',], sens['JGL',], alternative="greater")
+    pval <- sprintf(paste0("%.", prec + 1, "f"), t_test$p.value)
+    pval <- ifelse(round(t_test$p.value,prec+1) == 0, paste0(c("<0.",rep(0, prec), 1), collapse=""), pval)
+    # pval <- ifelse(t_test$p.value < 0.05, paste0("\\mathbf{", pval, "}"), pval)
+    pval <- paste0("$", pval, "$")
+    stat <- paste0("$", sprintf(paste0("%.", prec, "f"), t_test$statistic), "$")
+    degf <- paste0("$", sprintf(paste0("%.", prec, "f"), t_test$parameter), "$")
+    mean_covdepGE <- paste0("$", sprintf(paste0("%.", prec, "f"), t_test$estimate[1] * 100), "$")
+    # mean_JGL <- paste0("$", sprintf(paste0("%.", prec, "f"), t_test$estimate[2] * 100), "$")
+    se <- paste0("$", sprintf(paste0("%.", prec, "f"), t_test$stderr * 100), "$")
+    htest[[p]] <- data.frame(covdepGE=paste0("$", sens_str[row.names(sens) == "covdepGE"], "$"),
+                             JGL=paste0("$", sens_str[row.names(sens) == "JGL"], "$"),
+                             stderr=se, stat=stat, df=degf, p=pval)
+  }
+
   # process specificity results
   spec <- sapply(results, sapply, `[[`, "spec")
   spec_mean <- rowMeans(spec)
@@ -92,6 +115,12 @@ for (p in as.character(dims)){
   # process time results
   time <- sapply(results, sapply, `[[`, "time")
   time_mean <- rowMeans(time)
+  if (seq){
+    time_ratios <- time['covdepGE_seq',] / time['covdepGE',]
+    ratio_mean <- sprintf(paste0("%.", prec, "f"), mean(time_ratios))
+    ratio_sd <- sprintf(paste0("%.", prec, "f"), sd(time_ratios))
+    ratio_str <- paste0(ratio_mean, " (", ratio_sd, ")")
+  }
   if (med) time_mean <- apply(time, 1, median)
   min_time_ind <- which.min(time_mean)
   time_mean <- sprintf(paste0("%.", prec, "f"), time_mean)
@@ -109,6 +138,9 @@ for (p in as.character(dims)){
   df_exp <- data.frame(p = p, method = methods,
                        sens = NA, spec = NA, time = NA)
   df_exp[ , c("sens", "spec", "time")] <- perf_str[df_exp$method, ]
+  if (seq){
+    df_exp$time_ratio <- ratio_str
+  }
   df[[p]] <- df_exp
 
   subgroups[[p]] <- sapply(results, function(trial) length(unique(trial$JGL$classification)))
@@ -116,7 +148,21 @@ for (p in as.character(dims)){
   rm("results")
 }
 
-subgroups_list[[exper]] <- factor(Reduce(c, subgroups), levels = 2:6)
+if (seq){
+  df <- Reduce(rbind, df)
+  df <- df[,c("p", "method", "time", "time_ratio")]
+  df <- data.frame(p = df[df$method == "covdepGE", "p"],
+                   ptime = df[df$method == "covdepGE","time"],
+                   stime = df[df$method == "covdepGE_seq","time"],
+                   ratio = df[df$method == "covdepGE_seq","time_ratio"])
+  colnames(df) <- colnames(df) <- c("$p$", "Parallel Time (seconds)", "Sequential Time (seconds)", "Ratio")
+  kbl(df, format = "latex", booktabs = T, escape = FALSE) # %>%
+    collapse_rows(columns = c(1, 2, 3, 4), latex_hline = "major", valign = "middle")
+}
+
+htest_df <- Reduce(rbind, htest)
+colnames(htest_df) <- c("\\texttt{covdepGE}", "\\texttt{JGL}", "Standard Error", "T-statistic", "DF", "p-value")
+kbl(htest_df, format = "latex", booktabs = T, escape = FALSE)
 
 df <- Reduce(rbind, df)
 df$method <- gsub("_sortZ", "\\\\_time", df$method)
@@ -125,11 +171,14 @@ colnames(df) <- c("$p$", "Method", "Sensitivity$(\\%)$", "Specificity$(\\%)$", "
 kbl(df, format = "latex", booktabs = T, escape = FALSE) %>%
   collapse_rows(columns = c(1, 2, 3, 4), latex_hline = "major", valign = "middle")
 
+subgroups_list[[exper]] <- factor(Reduce(c, subgroups), levels = 2:6)
+
 windowsFonts("Times" = windowsFont("Times"))
+colors <- c("#BC3C29FF", "#0072B5FF", "#E18727FF", "#20854EFF")
 plots[[exper]] <- list(x = factor(subgroups, levels = 2:6), plot = NULL)
 plots <- list(ggplot() +
                 geom_bar(aes(x = subgroups_list[["cont_cov_dep_"]]),
-                         color = "black", fill = "#500000") +
+                         color = "black", fill = colors[1]) +
                 theme_pubclean() +
                 theme(text = element_text(family = "Times", size = 18),
                       plot.title = element_text(hjust = 0.5)) +
@@ -139,7 +188,7 @@ plots <- list(ggplot() +
                 labs(x = TeX("$\\textit{K}$")),
               ggplot() +
                 geom_bar(aes(x = subgroups_list[["cont_multi_cov_dep_"]]),
-                         color = "black", fill = "steelblue") +
+                         color = "black", fill = colors[2]) +
                 theme_pubclean() +
                 theme(text = element_text(family = "Times", size = 18),
                       plot.title = element_text(hjust = 0.5)) +
@@ -147,8 +196,8 @@ plots <- list(ggplot() +
                 scale_y_continuous(breaks = seq(0, 180, 30), limits = c(0, 180)) +
                 scale_x_discrete(drop=FALSE) +
                 labs(x = TeX("$\\textit{K}$")))
-ggarrange(plotlist = plots, nrow = 2)
-
+fig <- ggarrange(plotlist = plots, nrow = 2)
+ggsave("plots/subgroup_bar_newcol.pdf", height = 6, width = 11)
 
 # ------------------------------------------------------------------------------
 # HP comparisons
